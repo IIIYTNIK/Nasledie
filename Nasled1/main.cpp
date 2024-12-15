73f785a8c2083051922249a4edbc9d8503ff15c6
IIIYTNIL <denborodienko@yandex.ru>
2024-12-15 15:42:24 +0900

    Комментирование main.cpp

--- Nasled1/main.cpp
+++ Nasled1/main.cpp
@@ -6,28 +6,30 @@
 using namespace std;
 
 int main() {
+    //создание двух объктов класса гитары и 2 объекта класса смычковых инстумента
     Guitar electricGuitar("Electric Guitar", 6, "steel", "Alder", "Jumbo", "Wood");
     Guitar acousticGuitar("Acoustic Guitar", 6, "nylon", "Flame Maple", "Clasic", "Bone");
     BowedStringInstrument violin("Violin", 4, "gut", "wood", "Carbon");
     BowedStringInstrument cello("Cello", 4, "gut", "wood", "Fermabuk");
 
+    //настраиваем и играем на гитарах
+    electricGuitar.play();
     electricGuitar.tune();
     electricGuitar.play();
-    electricGuitar.play();
     cout << "\n";
-
     acousticGuitar.tune();
     acousticGuitar.play();
     cout << "\n";
-
+    //настраиваем и играем смычковые инструменты
     violin.tune();
     violin.play();
     cout << "\n";
-
     cello.tune();
     cello.play();
     cout << "\n";
 
+
+    //создаём вектор, в который запишем гитару и контрабас
     vector<StringInstrument*> instrument;
 
     instrument.push_back(new Guitar("Bass Guitar", 4, "steel", "Mahagony", "Jumbo", "Wood"));
